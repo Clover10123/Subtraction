c609c371f43be1b9583cc82baf9519725e1584ff
Clover <mooshicks@gmail.com>
2021-07-31 12:09:20 -0500

    Basic stage setting
    I included a main scene, settings panel, and game scene. It has no game logic other than some buttons and some UI tweens.

---
+++ Scenes/Scripts/MainScene.gd
@@ -0,0 +1,23 @@
+extends Node2D
+onready var alert = $AlertWindow/AlertText
+
+func _ready():
+	$AlertWindow.hide()
+
+func _on_PlayButton_pressed():
+	if get_tree().change_scene("res://GameScene.tscn") == OK:
+		return
+	else:
+		$AlertWindow.show()
+		update_alert_text("Error grabbing game scene.")
+
+
+func _on_Timer_timeout():
+	$AlertWindow.hide()
+	update_alert_text("")
+	pass # Replace with function body.
+
+func update_alert_text(msg:String):
+	alert.text = msg
+	if msg != "":
+		$Timer.start(4.0)
---
+++ Scenes/Scripts/SettingsMenu.gd
@@ -0,0 +1,42 @@
+extends Node2D
+onready var animator = $AnimationPlayer
+var quitprompt = false
+var settingsopen = false
+
+
+func _ready():
+	animator.play("default")
+	
+func _input(event):
+	if event.is_action_pressed("settings"):
+		settings_handler()
+
+func _on_QuitButton_pressed():
+	quit_handler()
+	pass # Replace with function body.
+
+
+func _on_ConfirmQuit_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_CancelQuit_pressed():
+	quit_handler()
+	pass # Replace with function body.
+
+func quit_handler():
+	if quitprompt:
+		animator.play("quitprompthide")
+		quitprompt = false
+	else:
+		animator.play("quitpromptshow")
+		quitprompt = true
+
+func settings_handler():
+	if settingsopen:
+		animator.play("settingspanelhide")
+		settingsopen = false
+	else:
+		animator.play("settingspanelshow")
+		settingsopen = true
